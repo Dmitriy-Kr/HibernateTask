0d11e5b8c44693e95fa3d7c8ddf05d4a63ba03de
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-15 17:31:49 +0300

    Add TraineeRepository interface

---
+++ src/main/java/edu/java/hibernatetask/repository/TraineeRepository.java
@@ -0,0 +1,20 @@
+package edu.java.hibernatetask.repository;
+
+import edu.java.hibernatetask.entity.Trainee;
+import edu.java.hibernatetask.entity.Training;
+import edu.java.hibernatetask.entity.TrainingType;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface TraineeRepository {
+    Optional<Trainee> save(Trainee trainee);
+    Optional<Trainee> usernameAndPasswordMatching(String userName, String password);
+    Optional<Trainee> getTraineeByUserName(String userName);
+    void changePassword(Trainee trainee);
+    Optional<Trainee> update(Trainee trainer);
+    boolean changeStatus(Trainee trainee);
+    void deleteByUsername(String Username);
+    List<Training> getTrainings(String traineeUsername, Date fromDate, Date toDate, String trainerName, TrainingType trainingType);
+}
